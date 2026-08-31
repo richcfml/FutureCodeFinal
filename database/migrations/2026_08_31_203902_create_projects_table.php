e0a09cd5f777ca226b62ea9370227588eebab0fa
Richard Ardila <richcfml@gmail.com>
2026-08-31 15:42:04 -0500

    Create project model to hold site customer data

---
+++ database/migrations/2026_08_31_203902_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('contact_name');
+            $table->string('contact_email');
+            $table->string('contact_number');
+            $table->string('description');
+            $table->string('status');
+            $table->string('priority');
+            $table->string('assigned_to');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
